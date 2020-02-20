8e95338182a92328dadb2027358fe804841a7249
Katie King <katharinemargaretking@gmail.com>
2020-02-20 16:50:55 +1100

    rails 5 init

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'doses/new'
+  get 'doses/create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
